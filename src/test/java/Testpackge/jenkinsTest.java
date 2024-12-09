78ea8146d53bb02b665fe13567596e6fe5802a81
Shaik. Khadar vali <khadarsk414@gmail.com>
2024-12-09 17:59:07 +0530

    second time

--- src/test/java/Testpackge/jenkinsTest.java
+++ src/test/java/Testpackge/jenkinsTest.java
@@ -4,6 +4,10 @@ public class jenkinsTest
 {
 	public static void main(String[] args) {
 		System.out.println("test for jenkins");
+	
+	    System.out.println("my feourity is colour is blue");
+	    
+	
 	}
 
 }
